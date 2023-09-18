6a9de593fd34dc0d9c4c27899d45762ed2a32859
douglas.carneiro <douglas.carneiro@ufba.br>
2023-09-17 21:42:10 -0300

    updating the post page

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -17,6 +17,8 @@ WHERE author = :author
 -- :name get-message :? :1
 -- :doc selects a message
 SELECT * from posts_with_meta
+    inner join (select id, parent from posts) as p using (id)
+    inner join reply_count using (id)
 where id = :id
 
 -- :name create-user!* :! :n
@@ -134,3 +136,23 @@ and poster = :user
 and id = :post
 order by posted_at asc
 limit 1
+
+-- :name get-replies :? :*
+select * from posts_with_meta
+    inner join (select id, parent from posts) as p using (id)
+    inner join reply_count using (id)
+where id IN (select id from posts
+            where parent = :id)
+
+-- :name get-parents
+SELECT * from posts_with_meta
+    inner join (select id, parent from posts) as p using (id)
+    inner join reply_count using (id)
+where id in (with recursive parents as
+             (select id, parent from posts
+                where id = :id
+              UNION
+              select p.id, p.parent from posts p
+                    inner join parents pp
+                    on p.id = pp.parent)
+            select id from parents)
--- src/clj/guestbook/routes/services.clj
+++ src/clj/guestbook/routes/services.clj
@@ -155,7 +155,21 @@
                     (response/bad-request
                      {:message
                       (str "Could not boost message: " post-id
-                           " as " (:login identity) (.printStackTrace e))}))))}}]]
+                           " as " (:login identity) (.printStackTrace e))}))))}}]
+     ["/replies"
+      {::auth/roles (auth/roles :message/get)
+       :get {:handler (fn [{{{:keys [post-id]} :path} :parameters}]
+                        (let [replies (msg/get-replies post-id)]
+                          (response/ok
+                           {:replies
+                            replies})))}}]
+     ["/parents"
+      {::auth/roles (auth/roles :message/get)
+       :get {:handler (fn [{{{:keys [post-id]} :path} :parameters}]
+                        (let [parents (msg/get-parents post-id)]
+                          (response/ok
+                           {:parents
+                            parents})))}}]]
 
     [""
      {::auth/roles (auth/roles :message/create!)
